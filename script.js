216d7c7d9411b1e33ca733eafa6594299e63253d
Sushant Sonawane <sushantsonawanex1@gmail.com>
2026-07-18 22:08:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Welcome to my AI Portfolio 🚀");
+const text = "Python Developer | AI Enthusiast | Future AI Engineer";
+let i = 0;
+
+function typeEffect() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, 70);
+    }
+}
+
+typeEffect();
